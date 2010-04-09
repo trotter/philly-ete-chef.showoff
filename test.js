986852e18e2d6713189adcf6ec57b5f133f7c73a
Trotter Cashion <cashion@gmail.com>
2010-04-09 10:42:49 -0400

    Add a slide height stop centering everything

---
+++ test.js
@@ -0,0 +1,15 @@
+function centerSlide(slide) {
+  var slide_content = $(slide).children(".content").first()
+  var height = slide_content.height()
+  var mar_top = (0.5 * parseFloat($(slide).height())) - (0.5 * parseFloat(height))
+  if (mar_top < 0) {
+    mar_top = 0
+  }
+  // TODO: Make this conditional on having the "center" class
+  //       ... or override when there's a top class
+  if (slide_content.hasClass("subsection") ||
+      slide_content.hasClass("title") ||
+      slide_content.hasClass("center")) {
+    slide_content.css('margin-top', mar_top)
+  }
+}
